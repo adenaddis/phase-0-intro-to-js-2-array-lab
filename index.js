e7398ab97173ebe58003918296a206e85fe196b4
Aden Addis <adenwonde56@gmail.com>
2022-05-26 21:55:01 -0400

    commit tree 2efef12e4573871f0c122a7e60c285a2b19d1ed5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+} 
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+} 
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+// function appendCat() {
+//     cats.slice();
+
+function appendCat() {
+    const CopyOfCats = cats.slice();
+    CopyOfCats.push('Broom');
+    return CopyOfCats
+}
+function prependCat() {
+    const CopyOfCats = cats.slice();
+    CopyOfCats.unshift('Arnold');
+    return CopyOfCats
+}
+function removeLastCat() {
+    const CopyOfCats = cats.slice();
+    CopyOfCats.pop();
+    return CopyOfCats
+}
+function removeFirstCat() {
+    const CopyOfCats = cats.slice();
+    CopyOfCats.shift();
+    return CopyOfCats
+}
